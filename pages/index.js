8dffc2065fd8f7984895fe8e492e63d37d371d16
farhadyanuar <farhad.yanuar@bithealth.co.id>
2025-10-02 15:06:02 +0700

    add pagg index

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// pages/index.js
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+export default function Home() {
+  return <SwaggerUI url="/api/swagger" />;
+}
